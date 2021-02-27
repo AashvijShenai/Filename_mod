5490e775b615285cc6f67d1a3f8fe1706d1c9195
AashvijShenai <aashvijshenai@gmail.com>
2021-02-26 20:11:19 -0800

    dir mod, . bugfix, caps fix

--- filename_modifier.py
+++ filename_modifier.py
@@ -1,33 +1,47 @@
 import os
 import re
 
-APPLY_TO_DIR_FLAG = 0
+#Apply name modifiers to sub directories as well?
+APPLY_TO_DIR_FLAG = 1
 
+#Root path
 path = "D:\\Music\\Rammstein"
+#Keywords to remove from name
 keys_rem = ["rammstein", "Rammstein", "doh", "ner", "esc"]
-keys_space = ["-","_"]
-
-os.chdir(path)
+#Keywords to replace with a space
+keys_space = ["_","-"]
 
 #Modify this function for specific use
-def modify(old_file, path):
-    new_file = old_file
+def modify(old_file):
+    ext_flag = 1
+    #No file extension/directory
+    if(old_file.rfind(".") == -1):
+        new_file = old_file
+        ext_flag = 0
+    else:    
+        ext = old_file[old_file.rfind("."):len(old_file)]   #Saving file extension separately
+        new_file = old_file.replace(ext,"")
+
     for i in keys_rem:
         new_file = new_file.replace(i, "")
     for i in keys_space:
         new_file = new_file.replace(i, " ")
 
-    new_file = " ".join(new_file.strip().capitalize().split())
-    
-    #print("Old file: ", old_file)
-    #print("new file: ", new_file,"\n")
-    #t = input("Proceed?")
+    #Capitalise the first word and leave the rest as is and trim spaces.
+    new_file = re.sub('([a-zA-Z])', lambda x: x.groups()[0].upper(), new_file, 1)
+    new_file = " ".join(new_file.strip().split())
+    if(ext_flag):
+        new_file = new_file + ext
+
     return new_file
 
-
+os.chdir(path)
 
 for (root_path, dirs, files) in os.walk(os.getcwd()):
-    #print("1:: ", root_path)
-    #print("2:: ", dirs)
     for file in files:
-        os.rename(root_path+"\\"+file, root_path+"\\"+modify(file, root_path))
+        os.rename(root_path+"\\"+file, root_path+"\\"+modify(file))
+
+if(APPLY_TO_DIR_FLAG):
+    for (root_path, dirs, files) in os.walk(os.getcwd()):
+        for dire in dirs:
+            os.rename(root_path+"\\"+dire, root_path+"\\"+modify(dire))
